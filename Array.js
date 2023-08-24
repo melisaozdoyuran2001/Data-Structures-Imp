5df6e10da9b15a4be910254e4c614ebf12cadf0e
melisaozdoyuran <melisaozdoyuran@hotmail.com>
2023-08-24 18:23:17 +0300

    edited array

--- Array.js
+++ Array.js
@@ -16,17 +16,34 @@ class Array {
 
     pop() { //remove last elemeent
         const lastItem = this.data[this.length-1]
-        delete this.data[this.length-1];
+        delete this.data[this.length-1] ;
         this.length-- ; 
         return lastItem; 
+    }
+
+    delete(index) { //delete a given index
+        const item = this.data[index];
+        this.shiftItems(index); 
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length-1 ; i++) {
+            this.data[i] = this.data[i+1];
+        }
+       delete this.data[this.length-1];
+       this.length--; 
     }
 }
 
 const exampleArray = new Array();
 console.log(exampleArray.length);
-exampleArray.push(1);
+exampleArray.push('my');
 console.log(exampleArray);
-exampleArray.push(2); 
+exampleArray.push('name'); 
 console.log(exampleArray);
 exampleArray.pop(); // remove last element
 console.log(exampleArray); 
+exampleArray.push('is'); 
+exampleArray.push('melisa'); 
+console.log(exampleArray); 
+
